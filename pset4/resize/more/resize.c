bf3e08ac3de8608440366d0c2793d7b185c06ebd
Aaron Alef <aaron.alef42@gmail.com>
2018-11-10 22:05:56 +0000

    Finished third resizing algorithm. WORKING SO FARgit add .git add . (more tests will follow )

--- pset4/resize/more/resize.c
+++ pset4/resize/more/resize.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -24,8 +25,44 @@ typedef struct
 // Implementation of averaging resizing algorithm
 void resize (RGBTRIPLE * inMatrix [], RGBTRIPLE * outMatrix [], int outWidth, int outHeight, double factor);
 
+// Creates an initialized Area struct
+Area getArea (int x, int y, double factor);
+
 // Calculates the average value for each pixel in the output BMP
 RGBTRIPLE calcPixel (RGBTRIPLE * inMatrix [], Area area);
+
+// Adds the colors (margin + contend) from all pixels in the field up in a Color struct
+void sumUpColor (Color * color, RGBTRIPLE * inMatrix [], Area area);
+
+// Adds all whole pixels in the field to the Color struct
+void sumUpWholePixels (Color * color, RGBTRIPLE * inMatrix [], Area area);
+
+// Checks wether field only contains one pixel
+bool fieldInPixel (Area area);
+
+// Checks wether margin's smaller than one column of pixels
+bool sideInPixel (double lowerVal, double upperVal);
+
+// Iterates over each pixel in the margin and calls addMargin
+void sumUpMargin (Color * color, RGBTRIPLE * inMatrix [], Area area);
+
+// Adds a pixel from the margin to the Color sum
+void addMargin (Color * color, RGBTRIPLE * inMatrix [], double x, double y, double percentage);
+
+// Adds a stripe (smaller but longer than one pixel) to the Color sum
+void addStripe (Color * color, RGBTRIPLE * inMatrix [], Area area, char dir);
+
+// Adds the RGB value of given pixel to given Color struct
+void addPixelToColor (Color * color, RGBTRIPLE pixel, double percentage);
+
+// Divides given color by given area and returns it in a new Color struct
+Color divColorByArea (Color colorSum, Area area);
+
+// Converts a Color to an RGBTRIPLE
+RGBTRIPLE getPixelFromColor (Color color);
+
+// Calculates the size of an area
+double calcArea (Area area);
 /*=====================================*/
 
 int main(int argc, char *argv[])
@@ -164,7 +201,7 @@ void resize (RGBTRIPLE * inMatrix [], RGBTRIPLE * outMatrix [], int outWidth, in
 // Done
 Area getArea (int x, int y, double factor)
 {
-    Area area
+    Area area =
     {
         .x1 = x / factor,
         .x2 = (x + 1) / factor,
@@ -182,24 +219,162 @@ RGBTRIPLE calcPixel (RGBTRIPLE * inMatrix [], Area area)
     return getPixelFromColor (divColorByArea (color, area));
 }
 
-// TODO: iterate over pixel + add color
+// Done
 void sumUpColor (Color * color, RGBTRIPLE * inMatrix [], Area area)
 {
+    if (fieldInPixel (area))
+    {
+        addMargin (color, inMatrix, area.x1, area.y1, calcArea (area));
+    }
+    else if (sideInPixel(area.x1, area.x2))
+    {
+        addStripe (color, inMatrix, area, 'x');
+    }
+    else if (sideInPixel(area.y1, area.y2))
+    {
+        addStripe (color, inMatrix, area, 'y');
+    }
+    else
+    {
+        sumUpWholePixels (color, inMatrix, area);
+        sumUpMargin (color, inMatrix, area);
+    }
+}
 
+// Done
+bool fieldInPixel (Area area)
+{
+    return (sideInPixel (area.x1, area.x2) && sideInPixel (area.y1, area.y2));
+}
+
+// Done
+bool sideInPixel (double lowerVal, double upperVal)
+{
+    return (ceil (upperVal) - floor (lowerVal) == 1);
+}
+
+// Done
+void sumUpWholePixels (Color * color, RGBTRIPLE * inMatrix [], Area area)
+{
+    for (int y = (int) ceil (area.y1); y < floor (area.y2); y++)
+    {
+        for (int x = (int) ceil (area.x1); x < floor (area.x2); x++)
+        {
+            addPixelToColor (color, inMatrix [x][y], 1.0);
+        }
+    }
+}
+
+// Done
+void sumUpMargin (Color * color, RGBTRIPLE * inMatrix [], Area area)
+{
+    double left = ceil (area.x1) - area.x1;
+    double right = area.x2 - floor (area.x2);
+    double top = ceil (area.y1) - area.y1;
+    double bottom = area.y2 - floor (area.y2);
+
+    // Add corner pixels
+    addMargin (color, inMatrix, area.x1, area.y1, left * top);
+    addMargin (color, inMatrix, area.x2, area.y1, top * right);
+    addMargin (color, inMatrix, area.x1, area.y2, right * bottom);
+    addMargin (color, inMatrix, area.x2, area.y2, bottom * left);
+
+    // Add left and right margin
+    for (double y = ceil (area.y1); y < floor (area.y2); y++)
+    {
+        addMargin (color, inMatrix, area.x1, y, left);
+        addMargin (color, inMatrix, area.x2, y, right);
+    }
+
+    // Add top and bottom margin
+    for (double x = ceil (area.x1); x < floor (area.x2); x++)
+    {
+        addMargin (color, inMatrix, x, area.y1, top);
+        addMargin (color, inMatrix, x, area.y2, bottom);
+    }
+}
+
+// Done
+void addMargin (Color * color, RGBTRIPLE * inMatrix [], double x, double y, double percentage)
+{
+    addPixelToColor (color, inMatrix [(int) floor (x)][(int) floor (y)], percentage);
+}
+
+// Done
+void addStripe (Color * color, RGBTRIPLE * inMatrix [], Area area, char dir)
+{
+    double left, right, top, bottom;
+
+    // For stripes with height smaller than one but longer than one pixel
+    // if so, top == bottom
+    if (dir == 'y')
+    {
+        top = area.y2 - area.y1;
+        left = ceil (area.x1) - area.x1;
+        right = area.x2 - floor (area.x2);
+
+        for (int x = (int) floor (area.x1); x < ceil (area.x2); x++)
+        {
+            double width = 1;
+            if (x == floor (area.x1)) width = left;
+            if (x == floor (area.x2)) width = right;
+
+            addPixelToColor (color, inMatrix [x][(int) floor (area.y1)], width * top);
+        }
+    }
+
+    // For stripes with breadth smaller than one but longer than one pixel
+    // if so, left == right
+    if (dir == 'x')
+    {
+        left = area.x2 - area.x1;
+        top = ceil (area.y1) - area.y1;
+        bottom = area.y2 - floor (area.y2);
+
+        for (int y = (int) floor (area.y1); y < ceil (area.y2); y++)
+        {
+            double height = 1;
+            if (y == floor (area.y1)) height = top;
+            if (y == floor (area.y2)) height = bottom;
+
+            addPixelToColor (color, inMatrix [(int) floor (area.x1)][y], height * left);
+        }
+    }
+}
+
+// Done
+void addPixelToColor (Color * color, RGBTRIPLE pixel, double percentage)
+{
+    color->r += pixel.rgbtRed * percentage;
+    color->g += pixel.rgbtGreen * percentage;
+    color->b += pixel.rgbtBlue * percentage;
 }
 
 // Done
 Color divColorByArea (Color colorSum, Area area)
 {
-    double percentage (calcArea (area));
+    double percentage = calcArea (area);
     Color color =
     {
         .r = colorSum.r / percentage,
         .g = colorSum.g / percentage,
         .b = colorSum.b / percentage
-    }
+    };
     return color;
 }
+
+// Done
+RGBTRIPLE getPixelFromColor (Color color)
+{
+    RGBTRIPLE pixel =
+    {
+        .rgbtRed = color.r,
+        .rgbtGreen = color.g,
+        .rgbtBlue = color.b
+    };
+    return pixel;
+}
+
 // Done
 double calcArea (Area area)
 {
