342f2d8a1331b7af2c4eaa1ee2ff9d24b69e85b6
Ulrich von Zadow <uzadow@libavg.de>
2018-12-01 10:06:07 +0100

    Removed psets I won't be working on.

--- pset6/sentimental/Cash/cash.py
+++
@@ -1,9 +0,0 @@
-from cs50 import *
-import math
-
-change = get_float("Change owed: ")
-while change <= 0:
-    change = get_float("Change owed: ")
-
-change = round(change * 100)
-print(math.floor(change / 25) + math.floor(change % 25 / 10) + math.floor(change % 25 % 10 / 5) + math.floor(change % 25 % 10 % 5))
